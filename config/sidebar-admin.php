01dfd20afc579073faded352138dbfc04a4c0c00
riski maulana rahman <riskimaulanarahman@gmail.com>
2023-06-10 21:51:37 +0800

    revisi2

--- config/sidebar-admin.php
+++ config/sidebar-admin.php
@@ -22,6 +22,18 @@ return [
             'route-name' => 'soales.index'
         ],
         [
+            'icon' => 'fa fa-book',
+            'title' => 'Video Puisi',
+            'url' => '/videopuisi',
+            'route-name' => 'videopuisi.index'
+        ],
+        [
+            'icon' => 'fa fa-book',
+            'title' => 'Biodata',
+            'url' => '/biodata',
+            'route-name' => 'biodata.index'
+        ],
+        [
             'icon' => 'fa fa-users',
             'title' => 'Kelola User',
             'url' => '/master-user',
